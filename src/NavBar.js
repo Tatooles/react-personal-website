54f8ea9c0da22d5b4ad8f32226dfa94a9f5d2dae
Kevin Tatooles <tatooles@wisc.edu>
2022-09-01 23:06:59 -0500

    Added logic for mobile menu bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,15 @@
+import { useState } from "react";
+
 const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
+        <>
         <div className="mx-auto grid grid-cols-12 content-center
                         h-28 p-2 border-2 border-teal-400">
             {/* This needs work, need a gap between the name and the buttons */}
@@ -7,8 +17,8 @@ const NavBar = () => {
             <a href="#" className="font-bold text-xl sm:text-2xl">KEVIN TATOOLES</a>
             </div>
             <div className="md:hidden col-span-6 flex justify-end">
-                <button data-collapse-toggle="navbar-default" type="button" class="p-2 ml-3 text-sm text-black rounded-lg focus:ring-2 focus:ring-black" aria-controls="navbar-default" aria-expanded="false">
-                    <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                <button onClick={toggleMenu} data-collapse-toggle="navbar-default" type="button" className="p-2 ml-3 text-sm text-black rounded-lg focus:ring-2 focus:ring-black" aria-controls="navbar-default" aria-expanded="false">
+                    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
             </div>
             <div className="hidden md:flex col-span-8 space-between justify-between">
@@ -19,6 +29,10 @@ const NavBar = () => {
                 <NavBarButton text="CONTACT"></NavBarButton>
             </div>
         </div>
+        {menuOpen &&
+        <div>this is the menu</div>
+        }
+        </>
     );
 };
 
