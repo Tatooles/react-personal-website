1ff3234e379812bf5d612c27aff605c0b5cf8db7
Kevin Tatooles <tatooles@wisc.edu>
2022-09-11 21:39:47 -0500

    Added some tests for fun

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NavBar from './NavBar';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders ABOUT in navbarr', () => {
+  render(<NavBar />);
+  const aboutElement = screen.getAllByText(/ABOUT/i);
+  expect(aboutElement.length === 2);
+})
+
+test('renders name', () => {
+  render(<NavBar />);
+  const nameElement = screen.getByText(/KEVIN TATOOLES/i);
+  expect(nameElement).toBeInTheDocument();
+})
